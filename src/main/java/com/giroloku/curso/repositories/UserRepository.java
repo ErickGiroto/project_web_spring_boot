b747d0f35993dbde92026dbe41d9c26f82228440
ErickGiroto <Erick_Giroto@hotmail.com>
2024-02-25 19:18:23 -0300

    Criando regras nas url usando Postman

--- src/main/java/com/giroloku/curso/repositories/UserRepository.java
+++ src/main/java/com/giroloku/curso/repositories/UserRepository.java
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.giroloku.curso.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-	
-
 }
