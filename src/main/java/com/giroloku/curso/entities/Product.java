a7e4f7c969753d8d61a846cc0715d423c9eaad97
ErickGiroto <Erick_Giroto@hotmail.com>
2024-03-01 22:52:48 -0300

    Criando Tabela para banco de dados part 06

--- src/main/java/com/giroloku/curso/entities/Product.java
+++ src/main/java/com/giroloku/curso/entities/Product.java
@@ -9,14 +9,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,14 +26,16 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
-	private Set<Category> categories  = new HashSet<>(); // Começando Vazia e não nula
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+	private Set<Category> categories = new HashSet<>(); // Começando Vazia e não nula
 
 	public Product() {
-		
+
 	}
-	
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
@@ -39,7 +43,7 @@ public class Product implements Serializable{
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
-		
+
 	}
 
 	public Long getId() {
@@ -102,15 +106,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
 
 }
