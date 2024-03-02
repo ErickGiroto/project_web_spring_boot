48a8e694fae4a544ccc720b2f1b1f0c4ad207cac
ErickGiroto <Erick_Giroto@hotmail.com>
2024-03-01 22:33:20 -0300

    Criando Tabela para banco de dados part 05

---
+++ src/main/java/com/giroloku/curso/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.giroloku.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.giroloku.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
