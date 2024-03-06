f03c7c24299c52bad814bd85826efad2da7d2bce
ErickGiroto <Erick_Giroto@hotmail.com>
2024-03-05 21:06:16 -0300

    Tratamento delete

--- src/main/java/com/giroloku/curso/services/UserService.java
+++ src/main/java/com/giroloku/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.giroloku.curso.entities.User;
 import com.giroloku.curso.repositories.UserRepository;
+import com.giroloku.curso.services.exceptions.DatabaseException;
 import com.giroloku.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
 	}
 
 	public User update(Long id, User obj) {
@@ -40,11 +51,11 @@ public class UserService {
 
 	}
 
-	private void updateData(User entity, User obj){
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-				
+
 	}
 
 }
